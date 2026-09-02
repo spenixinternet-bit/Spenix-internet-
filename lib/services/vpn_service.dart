ce61ddd422c3745e5e5f4996f78fac599891ff04
spenixinternet-bit <spenixinternet@gmail.com>
2026-09-02 16:09:01 +0300

    Update vpn_service.dart

--- lib/services/vpn_service.dart
+++ lib/services/vpn_service.dart
@@ -1,66 +1,24 @@
-import 'package:openvpn_flutter/openvpn_flutter.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
+name: spenix_internet
+description: Spenix Internet Service Provider App
+publish_to: 'none'
+version: 1.0.0+1
 
-class VpnService {
-  // ============================================================
-  // CHANGE THIS TO YOUR GATEWAY IP
-  // ============================================================
-  static const String gatewayIp = 'YOUR_GATEWAY_IP'; // 👈 CHANGE THIS!
+environment:
+  sdk: '>=3.0.0 <4.0.0'
 
-  static const String vpnPort = '1194';
-  static const String vpnProtocol = 'udp';
+dependencies:
+  flutter:
+    sdk: flutter
+  get: ^4.6.6
+  shared_preferences: ^2.2.2
+  uuid: ^4.2.1
+  http: ^1.2.2
+  openvpn_connect: ^0.0.15
+  connectivity_plus: ^5.0.2
 
-  // ============================================================
-  // CHECK IF PHONE HAS NETWORK BARS
-  // ============================================================
-  static Future<bool> hasNetworkBars() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult != ConnectivityResult.none;
-  }
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
 
-  // ============================================================
-  // CONNECT TO VPN
-  // ============================================================
-  static Future<void> connect({
-    required String username,
-    required String password,
-  }) async {
-    if (!await hasNetworkBars()) {
-      throw Exception('No network bars. Please make sure you have signal.');
-    }
-
-    final profile = '''
-client
-dev tun
-proto $vpnProtocol
-remote $gatewayIp $vpnPort
-resolv-retry infinite
-nobind
-persist-key
-persist-tun
-remote-cert-tls server
-cipher AES-256-CBC
-verb 3
-auth-user-pass
-<ca>
------BEGIN CERTIFICATE-----
-YOUR_CA_CERT_HERE
------END CERTIFICATE-----
-</ca>
-''';
-
-    await OpenVpnFlutter.startVpn(profile);
-  }
-
-  // ============================================================
-  // DISCONNECT FROM VPN
-  // ============================================================
-  static Future<void> disconnect() async {
-    await OpenVpnFlutter.stopVpn();
-  }
-
-  // ============================================================
-  // GET VPN STATUS
-  // ============================================================
-  static Stream<VpnStatus> get status => OpenVpnFlutter.status;
-}
+flutter:
+  uses-material-design: true
